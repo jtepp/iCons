59b0a8d04046a3bc77a4f19bfffe0b8ebb192ff7
Jacob Tepperman <jtepp@icloud.com>
2021-01-22 14:11:59 -0500

    Added live updating data to item list

---
+++ app/src/main/java/com/group724/icons/Item.java
@@ -0,0 +1,12 @@
+package com.group724.icons;
+
+
+public class Item {
+    private String name, category;
+    private int available;
+
+    Item()  {}
+    public String getName() {return name;}
+    public String getCategory() {return category;}
+    public int getAvailable() {return available;}
+};
--- app/src/main/java/com/group724/icons/ItemList.java
+++ app/src/main/java/com/group724/icons/ItemList.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
@@ -23,65 +24,111 @@ import android.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.auth.User;
 
 public class ItemList extends AppCompatActivity {
 
+
+    private CollectionReference ref = FirebaseFirestore.getInstance().collection("items");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String category = getIntent().getStringExtra("CATEGORY");
         setContentView(R.layout.activity_item_list);
-        ArrayList<Item> items = new ArrayList<Item>();
+        List<Item> items = new ArrayList<Item>();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         LinearLayout itemLayout = findViewById(R.id.itemLayout);
-
-        db.collection("items")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()){
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("TAG", document.getId() + " => " + document.getData());
-
-                               items.add(new Item(document.getString("name"), (int) Math.round(document.getDouble("available")),document.getString("category")));
-                            }
-
-                        } else {
-                            Log.w("TAG", "Error getting documents.", task.getException());
-                        }
+        ref.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                for (DocumentSnapshot d : value.getDocuments()){
+//                    Log.d("Document", d.getData().toString());
+                    if (d.getString("category").equalsIgnoreCase(category) || category.equalsIgnoreCase("all")) {
+                        Item doc = d.toObject(Item.class);
+                        Log.d("TAG", d.getId() + " => " + doc);
+                        itemLayout.addView(returnBtn(doc));
                     }
-                });
+                }
+            }
+        });
+//        db.collection("items")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()){
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//
+//                                Item doc = document.toObject(Item.class);
+////                                Log.d("TAG", document.getId() + " => " + doc);
+//                                itemLayout.addView(returnBtn(doc));
+//                            }
+//
+//                        } else {
+//                            Log.w("TAG", "Error getting documents.", task.getException());
+//                        }
+//                    }
+//                });
 
-        String category = getIntent().getStringExtra("CATEGORY");
+
 
         TextView listHeader = findViewById(R.id.listHeader);
         listHeader.setText(category);
         listHeader.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("FOIFJSDFJDS:KFDS:I", items.toString());
+
+//
+//                for (Item i : items) {
+//                    Button btn = new Button(getApplicationContext());
+//                    btn.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+//                    btn.setText("BUTTON: " + i.getName());
+//                    btn.setTag(i);
+//                    btn.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            Log.d("CLCJSDFKLDSJ", i.getCategory());
+//                        }
+//                    });
+//                    itemLayout.addView(btn);
+//                }
+
             }
         });
 
-        ListView itemList = findViewById(R.id.itemList);
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.activity_item_list, R.id.textView, items);
-        itemList.setAdapter(arrayAdapter);
+
+
+
+
+
+
+
+
+
+
+
+    }
+    Button returnBtn(Item i){
+        Button btn = new Button(getApplicationContext());
+        btn.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        btn.setText(i.getName()+" | "+i.getAvailable()+" available");
+        btn.setTag(i);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("CLCJSDFKLDSJ", i.getCategory());
+            }
+        });
+        return btn;
     }
 }
 
-
-
-class Item {
-    String name, category;
-    int available;
-
-    Item(String name, int available, String category){
-        this.name = name;
-        this.available = available;
-        this.category = category;
-    }
-    };
