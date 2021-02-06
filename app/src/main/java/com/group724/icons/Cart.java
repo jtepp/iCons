457ece3fbb39ea764419c2742e7206cc9e970e3d
Jacob Tepperman <jtepp@icloud.com>
2021-02-05 23:56:48 -0500

    added a card

--- app/src/main/java/com/group724/icons/Cart.java
+++ app/src/main/java/com/group724/icons/Cart.java
@@ -2,6 +2,8 @@ package com.group724.icons;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class Cart extends AppCompatActivity {
@@ -10,5 +12,11 @@ public class Cart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+        Context context = getApplicationContext();
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                "iconsPref", Context.MODE_PRIVATE);
+        String[] cartid = sharedPref.getString("cartid","").split(",");
+        String[] cartname = sharedPref.getString("cartname","").split(",");
+        String[] cartq = sharedPref.getString("cartq","").split(",");
     }
 }
