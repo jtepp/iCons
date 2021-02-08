a9d63143e4fd49167fdf36e0dfc6057604ffdca2
Jacob Tepperman <jtepp@icloud.com>
2021-02-08 16:27:26 -0500

    added sub to item model

--- app/src/main/java/com/group724/icons/Item.java
+++ app/src/main/java/com/group724/icons/Item.java
@@ -2,7 +2,7 @@ package com.group724.icons;
 
 
 public class Item {
-    private String name, category, ID;
+    private String name, category, sub, ID;
     private int available;
 
     Item()  {}
@@ -10,5 +10,6 @@ public class Item {
     public String getID() {return ID;}
     public void setID(String inp) {this.ID = inp;}
     public String getCategory() {return category;}
+    public String getSub() {return sub;}
     public int getAvailable() {return available;}
 };
