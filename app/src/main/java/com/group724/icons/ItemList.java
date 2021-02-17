d2139ccbcfe8ca4806428a6bcbc658d6e5f18b82
Jacob Tepperman <jtepp@icloud.com>
2021-02-17 13:17:37 -0500

    Migrating to ExpandableListView

--- app/src/main/java/com/group724/icons/ItemList.java
+++ app/src/main/java/com/group724/icons/ItemList.java
@@ -1,6 +1,7 @@
 package com.group724.icons;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import androidx.annotation.Nullable;
@@ -18,6 +19,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ExpandableListView.OnGroupClickListener;
 import android.widget.TextView;
@@ -45,10 +47,50 @@ public class ItemList extends AppCompatActivity {
 
     CollectionReference ref = FirebaseFirestore.getInstance().collection("items");
 
+    private AnimatedExpandableListView listView;
+    private ExampleAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String category = getIntent().getStringExtra("CATEGORY");
+
+        List<GroupItem> items = new ArrayList<GroupItem>();
+
+        for(int i = 1; i < 100; i++) {
+            GroupItem item = new GroupItem();
+
+            item.title = "Group " + i;
+
+            for(int j = 0; j < i; j++) {
+                ChildItem child = new ChildItem();
+                child.title = "Awesome item " + j;
+                child.hint = "Too awesome";
+
+                item.items.add(child);
+            }
+
+            items.add(item);
+        }
+
+        adapter = new ExampleAdapter(this);
+        adapter.setData(items);
+
+        listView = (AnimatedExpandableListView) findViewById(R.id.itemExpandableListView);
+        listView.setAdapter(adapter);
+
+        listView.setOnGroupClickListener(new OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                if (listView.isGroupExpanded(groupPosition)) {
+                    listView.collapseGroupWithAnimation(groupPosition);
+                } else {
+                    listView.expandGroupWithAnimation(groupPosition);
+                }
+                return true;
+            }
+        });
+
 
 //        if (getIntent().getExtras().containsKey("confirmed")){
 //            Snackbar result;
@@ -63,9 +105,9 @@ public class ItemList extends AppCompatActivity {
 //        }
 
         setContentView(R.layout.activity_item_list);
-        List<Item> items = new ArrayList<Item>();
+
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        LinearLayout itemLayout = findViewById(R.id.scrollLayout);
+//        LinearLayout itemLayout = findViewById(R.id.scrollLayout);
 
         FloatingActionButton cart = findViewById(R.id.toCartitemlist);
         cart.setOnClickListener(new View.OnClickListener() {
@@ -79,14 +121,14 @@ public class ItemList extends AppCompatActivity {
         ref.addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                itemLayout.removeAllViews();
+//                itemLayout.removeAllViews();
                 for (DocumentSnapshot d : value.getDocuments()){
 //                    Log.d("Document", d.getData().toString());
                     if (d.getString("category").equalsIgnoreCase(category) || category.equalsIgnoreCase("all")) {
                         Item doc = d.toObject(Item.class);
                         doc.setID(d.getId());
                         Log.d("TAG", d.getId() + " => " + doc);
-                        itemLayout.addView(returnBtn(doc));
+//                        itemLayout.addView(returnBtn(doc));
                     }
                 }
             }
@@ -116,5 +158,118 @@ public class ItemList extends AppCompatActivity {
         });
         return btn;
     }
+
+    private static class GroupItem {
+        String title;
+        List<ChildItem> items = new ArrayList<ChildItem>();
+    }
+
+    private static class ChildItem {
+        String title;
+        String hint;
+    }
+
+    private static class ChildHolder {
+        TextView title;
+        TextView hint;
+    }
+
+    private static class GroupHolder {
+        TextView title;
+    }
+
+    private class ExampleAdapter extends AnimatedExpandableListAdapter {
+        private LayoutInflater inflater;
+
+        private List<GroupItem> items;
+
+        public ExampleAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+        }
+
+        public void setData(List<GroupItem> items) {
+            this.items = items;
+        }
+
+        @Override
+        public ChildItem getChild(int groupPosition, int childPosition) {
+            return items.get(groupPosition).items.get(childPosition);
+        }
+
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            return childPosition;
+        }
+
+        @Override
+        public View getRealChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+            ChildHolder holder;
+            ChildItem item = getChild(groupPosition, childPosition);
+            if (convertView == null) {
+                holder = new ChildHolder();
+//                convertView = inflater.inflate(R.layout.list_item, parent, false);
+//                holder.title = (TextView) convertView.findViewById(R.id.textTitle);
+//                holder.hint = (TextView) convertView.findViewById(R.id.textHint);
+                convertView.setTag(holder);
+            } else {
+                holder = (ChildHolder) convertView.getTag();
+            }
+
+            holder.title.setText(item.title);
+            holder.hint.setText(item.hint);
+
+            return convertView;
+        }
+
+        @Override
+        public int getRealChildrenCount(int groupPosition) {
+            return items.get(groupPosition).items.size();
+        }
+
+        @Override
+        public GroupItem getGroup(int groupPosition) {
+            return items.get(groupPosition);
+        }
+
+        @Override
+        public int getGroupCount() {
+            return items.size();
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            return groupPosition;
+        }
+
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+            GroupHolder holder;
+            GroupItem item = getGroup(groupPosition);
+            if (convertView == null) {
+                holder = new GroupHolder();
+//                convertView = inflater.inflate(R.layout.group_item, parent, false);
+//                holder.title = (TextView) convertView.findViewById(R.id.textTitle);
+                convertView.setTag(holder);
+            } else {
+                holder = (GroupHolder) convertView.getTag();
+            }
+
+            holder.title.setText(item.title);
+
+            return convertView;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public boolean isChildSelectable(int arg0, int arg1) {
+            return true;
+        }
+
+    }
+
 }
 
