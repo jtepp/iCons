0290313b324adbdd2ddd3a25434766dcb1ed1ea0
Jacob Tepperman <jtepp@icloud.com>
2021-03-11 16:12:11 -0500

    added hours page

---
+++ app/src/main/java/com/group724/icons/Hours.java
@@ -0,0 +1,25 @@
+package com.group724.icons;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Hours extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hours);
+
+        Window window = Hours.this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(ContextCompat.getColor(Hours.this, R.color.red));
+    }
+}
